1785011ac7c48145e6e08def828fe5b246d7a79b
HaikalShamsudin <haikalbinshamsudin@gmail.com>
2024-02-07 00:05:36 +0800

    Initial commit

---
+++ src/Assignment1/Assignment1.java
@@ -0,0 +1,17 @@
+package Assignment1;
+
+public class Assignment1 {
+    
+    public class Assignment1 {
+    
+        public static void main(String[] args) {
+            
+            // Passing the argument to the main method.
+    
+            System.err.println("It have" + args.length + "arguments");
+    
+            // using 'for' loop
+            for(int i = 0;i < args.length;i++);
+            System.out.println((i+1)+"."args[i]);
+        }
+}
